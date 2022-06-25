1c082ecfc4fb9cd1952703c1b45ee7176f0782a7
Tanner <monokase18@gmail.com>
2022-06-25 20:44:58 +0900

    New Problem

---
+++ SWEA Level1/6312. 내장함수5 try except 연습하기.py
@@ -0,0 +1,17 @@
+
+
+
+# try랑 except를 연습시키는 건 맞는 것 같은데, 왜 except 문이 제대로 작동하지 않는지에 대해서는 좀 더 고민이 필요한 문제
+# def multiply(*args):
+#     c = 1
+#     try:
+#         for x in args:
+#             c = c*x
+#         print(c)
+#
+#     except NameError:
+#         print("에러발생")
+#
+# multiply(1, 2, 삼, 4)
+#
+
