ce572d6e3587ebd265b42a7bcde7ec05d2380640
Tanner <monokase18@gmail.com>
2022-06-27 06:22:25 +0900

    New Problem

--- 알고리즘/220627/SWEA 2072_홀수만 더하기_D1.py
+++ 알고리즘/220627/SWEA 2072_홀수만 더하기_D1.py
@@ -1,4 +1,15 @@
 
 T = int(input())
-
 for i in range(1, T+1):
+    a = list(map(int, input().split()))
+
+    b = []
+    for j in a:
+        if j % 2 != 0:
+            b.append(j)
+
+    print(f'#{i} {sum(b)}')
+
+## list 컴프리헨션으로 풀기 가능?
+
+# 생각해보자고...
