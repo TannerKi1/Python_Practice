1fe0b7a09fe175ed823f11654782fb039509b493
Tanner <monokase18@gmail.com>
2022-06-27 06:34:04 +0900

    New Problem

--- 알고리즘/220627/SWEA 2071_평균값 구하기_D1.py
+++ 알고리즘/220627/SWEA 2071_평균값 구하기_D1.py
@@ -1,10 +1,13 @@
 
 T = int(input())
-
+b = []
 for i in range (1, T+1):
     a = list(map(int, input().split()))
+    b.append(a)
+    print(f'#{i} {round(sum(a) / 10)}')
 
-    print(f'#{i} {round(sum(a)/10)}')
+print(b)
+
 
 
 # round() 함수 사용법 익히기
