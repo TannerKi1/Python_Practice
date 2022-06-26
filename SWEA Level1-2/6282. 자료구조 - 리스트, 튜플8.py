94241566131233ffa1633a5a0cddb7b81bbe9102
Tanner <monokase18@gmail.com>
2022-06-26 14:23:08 +0900

    New Problem

--- SWEA Level1-2/6282. 자료구조 - 리스트, 튜플8.py
+++ SWEA Level1-2/6282. 자료구조 - 리스트, 튜플8.py
@@ -1,11 +1,15 @@
-
-
-a = [1, 2, 3, 4, 5]
+k = [1, 3, 11, 15, 23, 28, 37, 52, 85, 100]
 b = []
-def check(a):
-    for x in a :
+
+
+def check(k):
+    for x in k:
         if x % 2 == 0:
             b.append(x)
+        else:
+            continue
+
     print(b)
 
-check(a)
+
+check(k)
