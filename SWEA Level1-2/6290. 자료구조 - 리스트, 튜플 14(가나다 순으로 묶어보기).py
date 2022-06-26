993cc6bd281bbaf435bb6e67c09bb6ae3b11c2b3
Tanner <monokase18@gmail.com>
2022-06-27 06:13:05 +0900

    New Problem

---
+++ SWEA Level1-2/6290. 자료구조 - 리스트, 튜플 14(가나다 순으로 묶어보기).py
@@ -0,0 +1,13 @@
+
+# append 로 푸는 방법이 있고...
+# https://totoma3.tistory.com/75
+
+a = []
+
+
+
+
+
+
+# 리스트 내포로 푸는 방법이 있고...
+# 출처 : https://taejin0527.github.io/[object%20Object]/Algorithm-SWEA-20190801-sw-6290/
