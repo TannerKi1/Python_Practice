8d1f144085547cccff88ba766b37bf4ba9c9e817
Tanner <monokase18@gmail.com>
2022-06-29 21:33:26 +0900

    New Problem

--- 알고리즘/220629/SWEA 6255_셋, 딕셔너리 3_D1(Value값 내림차순 정렬).py
+++ 알고리즘/220629/SWEA 6255_셋, 딕셔너리 3_D1(Value값 내림차순 정렬).py
@@ -4,11 +4,17 @@ my_dict = {"TV": 2000000, "냉장고": 1500000, "책상": 350000, "노트북": 1
 
 sorted_dict_2 = sorted(my_dict.items(), key = lambda item: item[1], reverse = True)
 
+print(my_dict.items())
+
+sorted_dic_3 = sorted(my_dict.items(), key = lambda item: item[0], reverse = False)
+
+print(sorted_dic_3)
+
 # 튜플 안의 값을 특정 기준으로 오름차순, 내림차순하는 연습문제라고 할 수 있다.
 # my.dict.items()가 무엇인지 부터 정리를 해야한다.
 # dict_items 뒤에 튜플 값을 반환해준다.
 # 여기에서 key item[0]은 품목이고, key item[1]은 가격이다.
-
+print("---*5")
 b = list(sorted_dict_2)
 for m, k in b:
     print(f'{m}: {k}')
