fbe1a21d9fa14380d62a6b0ec751b23f65380605
Tanner <monokase18@gmail.com>
2022-07-01 23:44:07 +0900

    New Problem

---
+++ 알고리즘/220701/SWEA_6241_문자열3_D1(슬라이스).py
@@ -0,0 +1,20 @@
+
+
+# URL 주소를 구분하래
+
+# 뭔가 '/'를 기준으로 슬라이싱 하는 것 아닐까?
+
+# find 함수를 써보자. http 뒤는 왼쪽부터, 뒤쪽은 rfind를 쓰면 될 것 같다.
+
+k = 'http://www.example.com/test?p=1&q=2'
+
+a = k[0:k.find(':', 0)]
+b = k[k.find('w', 0) : k.rfind('/', 0)]
+c = k[k.rfind('/', 0)+1:]
+
+print(f'protocol: {a}')
+print(f'host: {b}')
+print(f'others: {c}')
+
+
+
