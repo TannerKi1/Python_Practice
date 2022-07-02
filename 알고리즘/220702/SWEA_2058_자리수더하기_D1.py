89b44d548ffadf658ef273cc0b7b2212e4eaff18
Tanner <monokase18@gmail.com>
2022-07-02 16:14:22 +0900

    ㅑ

---
+++ 알고리즘/220702/SWEA_2058_자리수더하기_D1.py
@@ -0,0 +1,20 @@
+
+# 자리수 더하기
+
+# 목표 : 하나의 자연수를 입력 받아 각 자릿수의 합을 계산하는 프로그램을 작성하라
+
+
+# 숫자를 받는다, 문자열로 바꾼다, 훑는다, 개별 값을 다 더한다.
+
+T = int(input())
+
+T_str = str(T)
+
+array = []
+count = 0
+
+for x in T_str:
+    array.append(int(x))
+
+print(sum(array))
+
