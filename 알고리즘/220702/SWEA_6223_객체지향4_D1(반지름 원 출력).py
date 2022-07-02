370f1f30f6456c786ef40eceff3ef51c13dcbb99
Tanner <monokase18@gmail.com>
2022-07-02 19:53:58 +0900

    New Problem

--- 알고리즘/220702/SWEA_6223_객체지향4_D1(반지름 원 출력).py
+++ 알고리즘/220702/SWEA_6223_객체지향4_D1(반지름 원 출력).py
@@ -1,3 +1,9 @@
+class Circle:
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def __repr__(self):
+        return f'원의 면적: {3.14 * int(self.__radius) * int(self.__radius)}'
 
 
-
+print(Circle(2))
