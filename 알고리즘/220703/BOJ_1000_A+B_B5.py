cbdd48348f6b09045a475781a44614abcaf91982
Tanner <monokase18@gmail.com>
2022-07-03 21:23:53 +0900

    New Problem

---
+++ 알고리즘/220703/BOJ_1000_A+B_B5.py
@@ -0,0 +1,5 @@
+T = list(map(int, input().split()))
+
+A = T[0]
+B = T[1]
+print(A+B)
---
+++ 알고리즘/220703/BOJ_10869_사칙연산_B5.py
@@ -0,0 +1,8 @@
+T = list(map(int, input().split()))
+A = T[0]
+B = T[1]
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
