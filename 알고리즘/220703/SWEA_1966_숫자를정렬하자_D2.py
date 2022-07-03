c6331218212ae67ef70333c9a3dee38d7a53a78d
Tanner <monokase18@gmail.com>
2022-07-03 09:07:09 +0900

    New Problem

---
+++ 알고리즘/220703/SWEA_1966_숫자를정렬하자_D2.py
@@ -0,0 +1,15 @@
+
+T = int(input())
+N = int(input())
+
+for x in range(1, T+1):
+    b = list(map(int, input().split()))
+    b.sort()
+
+    print(f'#{x}', end=' ')
+    for j in range(0, N):
+        print(b[j], end=' ')
+    print()
+
+
+# 줄이 다른걸 print()로 마지막에 받으면 한 줄로 합쳐져서 나오나?
