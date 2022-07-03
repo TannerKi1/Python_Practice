180c4bb9ba901ca733e270fbabafb7237f24b327
Tanner <monokase18@gmail.com>
2022-07-04 00:52:29 +0900

    New Problem

--- 알고리즘/220704/BOJ_10809_알파벳찾기_B5.py
+++ 알고리즘/220704/BOJ_10809_알파벳찾기_B5.py
@@ -1,11 +1,6 @@
 S = str(input())
-
 k = [-1] * 26
-p = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+p = 'abcdefghijklmnopqrstuvwxyz'
 for i in range(0, 26):
     k[i] = S.find(p[i])
-
-for j in k:
-    print(j, end = ' ')
+    print(k[i], end=' ')
