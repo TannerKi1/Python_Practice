0e644c1423effe87686901edd58567824202a33d
Tanner <monokase18@gmail.com>
2022-07-03 16:36:01 +0900

    New Problem

---
+++ 알아놓으면 좋은 지식들/카운팅(계수)정렬_구현해보자.py
@@ -0,0 +1,27 @@
+
+# 카운팅 정렬 연습
+
+arr = [1, 1, 9, 2, 4, 7, 3, 5, 1]
+
+count = [0] * (max(arr) + 1)
+
+for x in arr:
+    count[x] += 1
+
+for k in range(1, len(count)):
+    count[k] = count[k-1] + count[k]
+
+print(count)
+
+result = [0] * (len(arr))
+
+print(result)
+
+for num in arr:
+    idx = count[num]
+    result[idx - 1] = num
+    count[num] -= 1
+    print(count)
+    print(result)
+
+
