c71008c2eac4723bc6f962d59ec809fb39603f7b
Tanner <monokase18@gmail.com>
2022-07-03 21:36:36 +0900

    New Problem

---
+++ 알고리즘/220703/BOJ_1008_A나누기B_B5.py
@@ -0,0 +1,4 @@
+T = list(map(int, input().split()))
+A = T[0]
+B = T[1]
+print(A/B)
---
+++ 알고리즘/220703/BOJ_9498_시험성적_B5.py
@@ -0,0 +1,12 @@
+testscore = int(input())
+
+if 90 <= testscore <= 100:
+    print('A')
+elif 80 <= testscore <= 89:
+    print('B')
+elif 70 <= testscore <= 79:
+    print('C')
+elif 60 <= testscore <= 69:
+    print('D')
+else:
+    print('F')
