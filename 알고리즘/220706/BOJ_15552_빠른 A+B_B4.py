e375ecc608306d8991bfd19157a50505948ac795
Tanner <monokase18@gmail.com>
2022-07-06 22:16:39 +0900

    New Problem

--- 알고리즘/220706/BOJ_15552_빠른 A+B_B4.py
+++ 알고리즘/220706/BOJ_15552_빠른 A+B_B4.py
@@ -1,7 +1,6 @@
 import sys
-
 T = int(input())
 
-for _ in range(T+1):
-    A, B = map(int, input().split())
+for _ in range(T):
+    A, B = map(int, sys.stdin.readline().split())
     print(A+B)
---
+++ 알고리즘/220706/BOJ_2562_최댓값_B3.py
@@ -0,0 +1,10 @@
+import sys
+
+arr = []
+
+for i in range(9):
+    a = int(sys.stdin.readline().strip())
+    arr.append(a)
+
+print(max(arr))
+print(arr.index(max(arr))+1)
