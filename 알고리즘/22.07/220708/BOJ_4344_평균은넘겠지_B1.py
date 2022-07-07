c29818c7b8085b91dcaa9ead1e5d2e19cd8a1838
Tanner <monokase18@gmail.com>
2022-07-08 00:37:33 +0900

    New Problem

---
+++ 알고리즘/22.07/220708/BOJ_4344_평균은넘겠지_B1.py
@@ -0,0 +1,18 @@
+C = int(input())
+
+for x in range(C):
+    score = list(map(int, input().split()))
+    N = score[0]
+    N_sum = sum(score[1::])
+    N_avg = N_sum / N
+    count = 0
+
+    for j in score:
+
+        if j > N_avg:
+            count += 1
+        else:
+            continue
+        answer = count / N * 100
+
+    print(f'{round(answer, 3):.3f}%')
