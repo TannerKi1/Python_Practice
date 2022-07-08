9de1058993a2808cb0707357e95ac2344d91717e
Tanner <monokase18@gmail.com>
2022-07-08 20:19:30 +0900

    New Problem

---
+++ 알고리즘/22.07/220708/BOJ_8958_OX퀴즈_B2.py
@@ -0,0 +1,13 @@
+test = 'OOXXOXXOOO'
+
+count = 0
+
+for i in range(len(test)):
+    for j in range(len(test-1)):
+        if test[i+j] == 'O':
+            count += 1
+        elif test[i+j] == 'X'
+            continue
+
+    elif test[i] == 'X':
+        continue
