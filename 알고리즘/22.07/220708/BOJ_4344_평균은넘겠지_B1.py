057bbf02dace1716c3a80dc3d73afd702d0510ae
Tanner <monokase18@gmail.com>
2022-07-08 20:22:43 +0900

    New Problem

--- 알고리즘/22.07/220708/BOJ_4344_평균은넘겠지_B1.py
+++ 알고리즘/22.07/220708/BOJ_4344_평균은넘겠지_B1.py
@@ -3,16 +3,17 @@ C = int(input())
 for x in range(C):
     score = list(map(int, input().split()))
     N = score[0]
+    N_rscore = score[1::]
     N_sum = sum(score[1::])
     N_avg = N_sum / N
     count = 0
 
-    for j in score:
+    for j in N_rscore:
 
         if j > N_avg:
             count += 1
         else:
             continue
-        answer = count / N * 100
+    answer = count / N * 100
 
     print(f'{round(answer, 3):.3f}%')
