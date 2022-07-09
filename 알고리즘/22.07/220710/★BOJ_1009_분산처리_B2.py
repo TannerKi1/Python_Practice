5bf8bbd7eecd3650b8ed6c3e93f5b9cf87a1a414
Tanner <monokase18@gmail.com>
2022-07-10 00:43:33 +0900

    New Problem

--- 알고리즘/22.07/220710/★BOJ_1009_분산처리_B2.py
+++ 알고리즘/22.07/220710/★BOJ_1009_분산처리_B2.py
@@ -3,13 +3,16 @@ T = int(input())
 
 # 제곱하고, 뒷자리 수만 챙긴다!
 for _ in range(T):
-    a, b = input().split()
+    a, b = map(int, input().split())
     a_int = int(a)
     b_int = int(b)
     for _ in range(b_int):
-        a_int = (a_int * a_int) % 10
+        if a_int < 10:
+            a_int = a_int * a_int
+
     print(a_int)
 
 
 
 
+3 9 7 1 3 9 7
