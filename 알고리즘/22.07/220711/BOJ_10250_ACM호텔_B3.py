cd62e9f991f0e6fee02345e8f7a07e6d52889378
Tanner <monokase18@gmail.com>
2022-07-11 00:50:30 +0900

    New Problem

---
+++ 알고리즘/22.07/220711/BOJ_10250_ACM호텔_B3.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for i in range(T):
+    H, W, N = map(int, input().split())
+    floor_room = N % H
+    if floor_room == 0:
+        end_room = N // H
+        floor_room = H
+        if end_room < 10:
+            print(int(str(floor_room) + '0' + str(end_room)))
+        elif end_room >= 10:
+            print(int(str(floor_room) + str(end_room)))
+    elif floor_room != 0:
+        end_room = N // H + 1
+        if end_room < 10:
+            print(int(str(floor_room) + '0' + str(end_room)))
+        elif end_room >= 10:
+            print(int(str(floor_room) + str(end_room)))
+
