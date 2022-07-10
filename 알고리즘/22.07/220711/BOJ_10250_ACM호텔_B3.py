29433a061021444417008b2df0d71d79928f0ac0
Tanner <monokase18@gmail.com>
2022-07-11 05:28:30 +0900

    New Problem

--- 알고리즘/22.07/220711/BOJ_10250_ACM호텔_B3.py
+++ 알고리즘/22.07/220711/BOJ_10250_ACM호텔_B3.py
@@ -8,12 +8,12 @@ for i in range(T):
         floor_room = H
         if end_room < 10:
             print(int(str(floor_room) + '0' + str(end_room)))
-        elif end_room >= 10:
+        else:
             print(int(str(floor_room) + str(end_room)))
     elif floor_room != 0:
         end_room = N // H + 1
         if end_room < 10:
             print(int(str(floor_room) + '0' + str(end_room)))
-        elif end_room >= 10:
+        else:
             print(int(str(floor_room) + str(end_room)))
 
