6ea5b1c8351946c878db0db681ffa74d6653e872
Tanner <monokase18@gmail.com>
2022-07-12 20:41:28 +0900

    d

--- 알고리즘/22.07/220712/LeetCode_1.TwoSum_Easy.py
+++ 알고리즘/22.07/220712/LeetCode_1.TwoSum_Easy.py
@@ -1,2 +1,9 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+
+# 하지만 for문을 2번 돌렸기 때문에 시간 복잡도는 n**2로 측정된다.
+# 더 효율적인 풀이가 아래에 있다.
+
+        for x in range(len(nums)):
+            for y in range(x + 1, len(nums)):
+                if nums[x] + nums[y] == target:
+                    return [x, y]
