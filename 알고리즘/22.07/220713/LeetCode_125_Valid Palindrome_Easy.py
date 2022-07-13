e806e2b4f35665057b1fcaa1d51b9c6c5e15665d
Tanner <monokase18@gmail.com>
2022-07-13 20:46:26 +0900

    d

--- 알고리즘/22.07/220713/LeetCode_125_Valid Palindrome_Easy.py
+++ 알고리즘/22.07/220713/LeetCode_125_Valid Palindrome_Easy.py
@@ -28,5 +28,21 @@ else:
 # 투포인터로 왼쪽 맨 끝고, 오른쪽 맨 끝에서부터 출발해 이 문제를 풀의한 풀이가 있다.
 # 투포인터는 반드시 인지하도록 하자
 
+cama!c
+왼쪽, 오른쪽에서 접근
 
+만약 isalnum이 아니면, 왼쪽에선 오른쪽으로 하나 보내고, 오른쪽에선 왼쪽으로 하나 보내주고
 
+### 참고 코드 ###
+# 안 보고 아래와 같이 투 포인터 작성할 수 있어야 함
+def isPalindrome(self, s):
+    l, r = 0, len(s)-1
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l <r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l +=1; r -= 1
+    return True
