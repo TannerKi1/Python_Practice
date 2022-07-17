2022ab8ea2eb0d34798c510424eda50a881a66df
Tanner <monokase18@gmail.com>
2022-07-17 17:05:19 +0900

    New Problem

--- 알고리즘/22.07/220717/SWEA_4843_특별한정렬_D3.py
+++ 알고리즘/22.07/220717/SWEA_4843_특별한정렬_D3.py
@@ -6,3 +6,24 @@
 
 # 그 다음에 큰 거에서 먼저 1개 작은 거에서 먼저 한 개 보내서 새로운 리스트를 만든다
 
+T = int(input())
+for x in range(1, T+1):
+    N = int(input())
+    unsorted_list = list(map(int, input().split()))
+
+    unsorted_list.sort()
+
+    sorted_list_A = unsorted_list[:N//2]
+    sorted_list_B = unsorted_list[N//2:]
+
+    answer_list = []
+
+    i, j = 0, len(sorted_list_B)-1
+    for _ in range(N//2):
+        answer_list.append(sorted_list_B[j])
+        answer_list.append(sorted_list_A[i])
+        i += 1
+        j -= 1
+
+    for x in range(10):
+        print(answer_list[x], end= ' ')
