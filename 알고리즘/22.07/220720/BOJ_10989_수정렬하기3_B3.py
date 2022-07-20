c293205256bc636c87047b7a0e0622a6071b9cc9
Tanner <monokase18@gmail.com>
2022-07-20 11:18:27 +0900

    New Problem

--- 알고리즘/22.07/220720/BOJ_10989_수정렬하기3_B3.py
+++ 알고리즘/22.07/220720/BOJ_10989_수정렬하기3_B3.py
@@ -1,1 +1,18 @@
+import sys
+N = int(input())
+
+arr = [0] * 10001
+
+# 0 , 1, 2, 3, 4, 5 ,...
+
+for _ in range(N):
+    num = int(sys.stdin.readline().strip())
+    arr[num-1] += 1
+
+
+for i in range(1, 10001):
+    while arr[i-1] > 0: #0부터 9999니까, 1부터 10000까지는 다 카운팅 될 수 있음!
+        print(i)
+        arr[i-1] -= 1
+
 
