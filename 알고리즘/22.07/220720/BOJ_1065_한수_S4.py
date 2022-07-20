285e36b5d309c4b2ce0102e40549d6b56c048eae
Tanner <monokase18@gmail.com>
2022-07-20 16:18:18 +0900

    New Problem

--- 알고리즘/22.07/220720/BOJ_1065_한수_S4.py
+++ 알고리즘/22.07/220720/BOJ_1065_한수_S4.py
@@ -6,23 +6,22 @@
 # 중간에 != 뜨면 break 뜨면서 while 탈출시키기
 
 N = str(input())
-
+count = 0
 num_list = list(map(int, N))  # 리스트랑 맵이용해서 자유자재로 바꾸기
 
 def checker(num_list):
-    count = 0
     i = 0
     if len(num_list) <= 2:
-        return "등차수열입니다"
+        return 1
     if len(num_list) >= 3:
         while (i + 2) <= (len(num_list) - 1) :
             if num_list[i] - num_list[i+1] != num_list[i+1] - num_list[i+2]:
-                return "등차수열이 아닙니다"
+                return 0
             else:
                 i += 1
-        return "등차수열입니다"
+        return 1
 
 
-print(checker(num_list))
+
 
 
