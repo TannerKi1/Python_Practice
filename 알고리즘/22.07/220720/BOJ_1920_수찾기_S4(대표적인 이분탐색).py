41149e74d3fb3a171c65bb3e19d9ef81a80b43fa
Tanner <monokase18@gmail.com>
2022-07-20 12:49:21 +0900

    New Problem

---
+++ 알고리즘/22.07/220720/BOJ_1920_수찾기_S4(대표적인 이분탐색).py
@@ -0,0 +1,30 @@
+import sys
+N = int(input())
+sort_arr = list(map(int, sys.stdin.readline().split()))
+M = int(input())
+target_arr = list(map(int, sys.stdin.readline().split()))
+
+
+sort_arr.sort()
+
+for x in target_arr:
+    left = 0
+    right = len(sort_arr) - 1
+    isExist = False
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if x == sort_arr[mid]:
+            isExist = True
+            print(1)
+            break
+
+        elif x < sort_arr[mid]:
+            right = mid - 1
+
+        elif x > sort_arr[mid]:
+            left = mid + 1
+
+    if not isExist:
+        print(0)
