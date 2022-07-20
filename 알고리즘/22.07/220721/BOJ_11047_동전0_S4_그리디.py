8cd0f6027623873ef5b996a4fe5e3b2aea47f4c3
Tanner <monokase18@gmail.com>
2022-07-21 00:32:10 +0900

    New Problem

---
+++ 알고리즘/22.07/220721/BOJ_11047_동전0_S4_그리디.py
@@ -0,0 +1,17 @@
+# 그리디 대표적인 문제
+
+N, K = map(int, input().split())
+money_2 = []
+for _ in range(N):
+    money_2.append(int(input()))
+
+money_2.sort(reverse = True)
+count = 0
+
+for coin in money_2:
+    if K >= coin: # 등호 조심!
+        add_count = K // coin
+        count += add_count
+        K %= coin
+
+print(count)
