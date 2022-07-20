4564bdd7d9478211f27ba0a44d03eb57f721a4b8
Tanner <monokase18@gmail.com>
2022-07-21 06:36:14 +0900

    New Problem

--- 알고리즘/22.07/220721/BOJ_1182_부분수열의합_S2.py
+++ 알고리즘/22.07/220721/BOJ_1182_부분수열의합_S2.py
@@ -13,7 +13,7 @@ for i in range(1<<i):
     mini = []
     for j in range(len(arr)):
         if i & (1<<j) == 0:
-            mini.append(arr[j])
+            mini.append(arr[j])  # 이 구문은 외워놓기. i는 전체 수, j는 len으로 받아서 이진법 진수 체크
     arr_3.append(mini)
 
 count = 0
