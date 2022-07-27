c87270d1472c16123743fa42edf0b334fd9774c2
Tanner <monokase18@gmail.com>
2022-07-28 00:33:31 +0900

    New Problem

--- 알고리즘 (날짜별정렬)/22.07/220728/BOJ_1620_나는야포켓몬마스터이다솜_S4_맵.py
+++ 알고리즘 (날짜별정렬)/22.07/220728/BOJ_1620_나는야포켓몬마스터이다솜_S4_맵.py
@@ -7,7 +7,7 @@ for x in range(N):
     dex.append(sys.stdin.readline().strip())
 
 dict = {dex[x]: x+1 for x in range(len(dex))} # 여기서 x가 이미 int 값으로 바뀌어서 value로 들어갔고...
-dict_2 = {v:k for k, v in dict.items()}
+dict_2 = {v:k for k, v in dict.items()} # key랑 value를 바꿔서 저장하는 법은 반드시 알아놓도록.
 
 for y in range(M):
     t = sys.stdin.readline().strip() # 여기서 주어지는 t는 str값이니까
