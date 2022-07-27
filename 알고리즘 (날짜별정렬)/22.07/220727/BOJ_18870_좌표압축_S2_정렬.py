cb405d4a07783fd50c7475c33213b4ecbe6bbc94
Tanner <monokase18@gmail.com>
2022-07-27 19:15:49 +0900

    New Problem

--- 알고리즘 (날짜별정렬)/22.07/220727/BOJ_18870_좌표압축_S2_정렬.py
+++ 알고리즘 (날짜별정렬)/22.07/220727/BOJ_18870_좌표압축_S2_정렬.py
@@ -1,13 +1,33 @@
 N = int(input())
 arr = list(map(int, input().split()))
 arr_2 = arr.copy()
-
-arr_3 = sorted(arr)
-arr_4 = sorted(list(set(arr_3))) # set을 하니 오름차순이 풀려버리는 문제점이 발생함.
-
-for x in arr_2:
-    print(arr_4.index(x), end= ' ')
+#
+# arr_3 = []
+#
+# for x in arr:
+#     if x not in arr_3:
+#         arr_3.append(x)
+#     else:
+#         continue
+#
+# arr_4 = sorted(arr_3)
+#
+# for x in arr_2:
+#     print(arr_4.index(x), end= ' ')
 
 # 역시 시간초과가 뜬다...
 
+# dictionary로 치면?
 
+# list와 dictionary의 순회 차이를 이해하고 있는지?...
+
+arr_3 = list(sorted(set(arr)))
+
+dict = {arr_3[i]:i for i in range(len(arr_3))} # 이거 잘 알아놓자. arr_3의 index값을 key로 부르고, value는 index 그 자체를 range만큼 반복함!
+
+
+for x in arr_2:
+    print(dict[x], end = ' ')    # x를 넣었을 때 그 value값인 index를 불러오게 된다.
+
+
+# 결론 : 딕셔너리의 시간 복잡도가 리스트보다 우수하다.
