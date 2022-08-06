017017b6e9d4c2e492aa34f97916f6188e837dec
TannerKi1 <monokase18@gmail.com>
2022-08-06 17:08:05 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220806/BOJ_1012_유기농배추_S1_DFS.py
+++ 날짜별 문제풀이/22.08/220806/BOJ_1012_유기농배추_S1_DFS.py
@@ -1,3 +1,15 @@
 T = int(input())
 for tc in range(T):
-    M, N, K = map(int, input())
+    M, N, K = map(int, input().split())
+
+# M 가로, N 세로
+
+graph = [[0] * M for _ in range(N)]
+
+for _ in range(K):
+    m, n = map(int, input().split())
+    graph[n][m] = 1  # 가로세로가 뒤집어 지게 된다.
+
+print(graph)
+
+
