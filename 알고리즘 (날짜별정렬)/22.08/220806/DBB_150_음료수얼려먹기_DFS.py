6357cc18a262f645a1b862bb9c9b5130b40bcb56
Tanner <monokase18@gmail.com>
2022-08-06 10:50:02 +0900

    New Problem

--- 알고리즘 (날짜별정렬)/22.08/220806/DBB_150_음료수얼려먹기_DFS.py
+++ 알고리즘 (날짜별정렬)/22.08/220806/DBB_150_음료수얼려먹기_DFS.py
@@ -1,7 +1,28 @@
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 
 graph = []
-for i in range(N):
+for i in range(n):
     graph.append(list(map(int, input())))
 
-print(graph) # 따로 split을 하지 않아도 개별 얼음틀처럼 들어가는 것을 확인할 수 있음.
+
+def dfs(x, y):
+    if x <= - 1 or x >= n or y <= - 1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+        return True # 한 번 돌 때 채울 수 있는 건 다 채웠기 때문에 인접한 0들은 False로 처리되게 됨.
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            print(i, j)
+            result += 1
+
+
