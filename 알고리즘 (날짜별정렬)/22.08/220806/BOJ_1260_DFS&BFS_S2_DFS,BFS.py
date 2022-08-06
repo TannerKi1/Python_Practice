7c2e1a067f9784db5a3daf1c4d5f7b2d06e2e412
Tanner <monokase18@gmail.com>
2022-08-06 11:23:35 +0900

    New Problem

--- 알고리즘 (날짜별정렬)/22.08/220806/BOJ_1260_DFS&BFS_S2_DFS,BFS.py
+++ 알고리즘 (날짜별정렬)/22.08/220806/BOJ_1260_DFS&BFS_S2_DFS,BFS.py
@@ -2,8 +2,7 @@ from collections import deque
 
 N, M, V = map(int, input().split())
 
-graph = [[] for _ in range(N+1)]
-
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     j, k = map(int, input().split())
@@ -12,39 +11,41 @@ for _ in range(M):
 
 for i in range(len(graph)):
     graph[i].sort()
+
+
 # print(graph) 여기까지 노드는 구현완료함.
 # 내부 그래프를 다시 sort해줘야 오름차순으로 방문할 수 있게 됨.
 
 # DFS부터 구현해보기 (재귀함수로)
-
-visited_d = [False] * (N + 1)
-
 def dfs(graph, V, visited_d):
     if not visited_d[V]:
         visited_d[V] = True
-        print(V, end= ' ')
+        print(V, end=' ')
 
         for i in graph[V]:
             if not visited_d[i]:
                 dfs(graph, i, visited_d)
 
-# BFS 구현해보기 (덱으로)
 
-visited_b = [False] * (N + 1)
+visited_d = [False] * (N + 1)
 
+dfs(graph, V, visited_d)
+print()
+
+
+# BFS 구현해 보기 (덱으로)
 def bfs(graph, V, visited_b):
     queue = deque([V])
     visited_b[V] = True
     while queue:
         v = queue.popleft()
-        print(v, end = ' ')
-        for i in graph[v]:
+        print(v, end=' ')
+        for i in graph[v]: # popleft를 하면서 이 친구의 연결된 다른 노드를 가져오는 것. 아까는 V로 고정되어있었으니, 3, 1, 4에서 끝났던 것
             if not visited_b[i]:
                 queue.append(i)
                 visited_b[i] = True
 
-# 인쇄하기
-dfs(graph, V, visited_d)
-print()
-bfs(graph, V, visited_b)
 
+visited_b = [False] * (N + 1)
+
+bfs(graph, V, visited_b)
