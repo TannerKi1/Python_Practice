e31cd1b3643682dbfbdd5955bc2203e8fbf3f932
Tanner <monokase18@gmail.com>
2022-08-07 10:25:39 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220807/BOJ_2178_미로탐색_S1_BFS.py
+++ 날짜별 문제풀이/22.08/220807/BOJ_2178_미로탐색_S1_BFS.py
@@ -1,2 +1,45 @@
 # 방문할 때 마다 + 1 올려서 최댓값 찾는 프로젝트
+from collections import deque
+
+N, M = map(int, input().split())
+
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input()))) #지도 생성 완료
+
+# 개별 격자를 좌표값으로 받아서 queue에 넣어주기
+
+# 4방향 지시표 만들어주기
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
+                continue # pass랑 continue차이 다시 생각해보기
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[N-1][M-1]
+
+
+print(bfs(0, 0))
+
+
+
 
---
+++ 날짜별 문제풀이/22.08/220807/SWEA_4871_그래프경로_D2_DFS.py
@@ -0,0 +1,38 @@
+TC = int(input())
+
+for tc in range(TC):
+    V, E = map(int, input().split())
+    graph = []
+
+    for _ in range(V+1):
+        graph.append([])
+
+    for _ in range(E):
+        m, n = map(int, input().split())
+        graph[m].append(n) # 이 문제에서는 단방향과 양방향을 구분함.
+
+    p, q = map(int, input().split())
+
+    visited = [False] * (V+1)
+
+    answer = list()
+    def dfs(graph, v, visited):
+        visited[v] = True
+        answer.append(v)
+        for i in graph[v]:
+            if not visited[i]:
+                dfs(graph, i, visited)
+
+        if q in answer:
+            return 1
+        else :
+            return 0
+    print(f'#{tc+1}', end = ' ')
+    print(dfs(graph, p, visited))
+
+
+
+
+
+
+
