ee59388711d381a266912a0e4caca2de1743cbea
Tanner <monokase18@gmail.com>
2022-08-08 08:54:53 +0900

    Greedy

--- 날짜별 문제풀이/22.08/220808/DBB_96_숫자카드게임_그리디.py
+++ 날짜별 문제풀이/22.08/220808/DBB_96_숫자카드게임_그리디.py
@@ -8,6 +8,9 @@ answer = 0
 for x in range(N):
     answer = max(answer, min(card_deck[x]))
     # max 함수를 통해, 둘 중 큰 것으로 for문 밖의 answer를 초기화시켜줌
+    # 굳이 list를 만들어서 하나하나 넣어서 공간낭비를 할 필요가 없다
+
+    # list에 일일히 행마다 최소값을 넣어줄 필요없이, 다음 줄로 넘어갈 때 갱신시켜주는 게 나음.
 
 print(answer)
 
