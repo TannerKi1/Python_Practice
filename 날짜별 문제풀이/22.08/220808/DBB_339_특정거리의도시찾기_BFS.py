a242ea4801325aeba43053891a3c7941ed977ff5
Tanner <monokase18@gmail.com>
2022-08-08 22:54:09 +0900

    New Problem

---
+++ 날짜별 문제풀이/22.08/220808/DBB_339_특정거리의도시찾기_BFS.py
@@ -0,0 +1,46 @@
+from collections import deque
+N, M, K, X = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+
+
+for _ in range(M):
+    j, k = map(int, input().split())
+    graph[j].append(k)
+
+visited = [False] * (N + 1)
+answer = []
+
+def bfs(graph, X, visited):
+    queue = deque([(X, 0)])
+    visited[X] = True
+
+    while queue:
+        (k, j) = queue.popleft()
+        answer.append((k, j))
+
+        for i in graph[k]:
+            if not visited[i]:
+                queue.append((i, j+1))
+                visited[i] = True
+
+
+bfs(graph, X, visited)
+
+arr = []
+for x in answer:
+    if x[1] == K:
+        arr.append(x[0])
+
+if len(arr) >= 1:
+    for k in arr:
+        print(k)
+
+if len(arr) == 0:
+    print(-1)
+
+
+
+
+
+
