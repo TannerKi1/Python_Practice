54c457d09be02e51290a38d518301bd732db7c6e
Tanner <monokase18@gmail.com>
2022-08-08 10:43:54 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220808/DBB_115_왕실의나이트_구현.py
+++ 날짜별 문제풀이/22.08/220808/DBB_115_왕실의나이트_구현.py
@@ -6,7 +6,7 @@ dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
 k = input()
 
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] #ord값으로 처리해서 숫자로 바꿀 수도 있었음.
+alpha = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] #ord값으로 처리해서 숫자로 바꿀 수도 있었음.
 
 x = int(k[1])
 y = alpha.index(k[0])
@@ -16,7 +16,7 @@ for i in range(8):
     nx = x + dx[i]
     ny = y + dy[i]
 
-    if nx < 1 or nx > 8 or ny < 0 or ny > 7:
+    if nx < 1 or nx > 8 or ny < 1 or ny > 8:
         continue
 
     cnt += 1
