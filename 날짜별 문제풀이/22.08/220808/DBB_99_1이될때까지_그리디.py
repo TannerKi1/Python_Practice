8578c3978a1a4070a6a8d7ee9cb88c2359d89c99
Tanner <monokase18@gmail.com>
2022-08-08 09:33:32 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220808/DBB_99_1이될때까지_그리디.py
+++ 날짜별 문제풀이/22.08/220808/DBB_99_1이될때까지_그리디.py
@@ -1,2 +1,16 @@
 N, K = map(int, input().split())
 
+d = [0] * (N+1)
+
+for i in range(2, N+1):
+    d[i] = d[i-1] + 1
+
+    if i % K == 0:
+        d[i] = min(d[i], d[i // K] + 1)
+
+    # d[4]가 처음에는 d[3] + 1을 거려 3으로 세팅되었다가
+    # if문에 걸리면서  min(3, 2)에 걸리면서 1로 초기화 완료
+    # 그 다음 d[5]는 d[4]값을 받게 됨.
+
+print(d[N])
+
