10e35248290912ae230ebd17435989b86675a8b5
Tanner <monokase18@gmail.com>
2022-08-09 17:32:09 +0900

    New Problem

--- ★기본개념/딕셔너리(map구조).py
+++ ★기본개념/딕셔너리(map구조).py
@@ -1,11 +1,17 @@
-arr = [1, 2, 3, 1, 2]
+# arr = [1, 2, 3, 1, 2]
+#
+# arr_2 = dict()
+#
+# for char in arr:
+#     if char not in arr_2:
+#         arr_2[char] = 1
+#     else:
+#         arr_2[char] += 1
+#
+# print(arr_2)
+#
 
-arr_2 = dict()
+arr = dict()
 
-for char in arr:
-    if char not in arr_2:
-        arr_2[char] = 1
-    else:
-        arr_2[char] += 1
+arr['파이썬'] = 2
 
-print(arr_2)
