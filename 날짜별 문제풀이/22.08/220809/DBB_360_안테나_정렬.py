8f2e09f44b83aee19ce9940d0189e41f548b7ddb
Tanner <monokase18@gmail.com>
2022-08-09 18:53:12 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220809/DBB_360_안테나_정렬.py
+++ 날짜별 문제풀이/22.08/220809/DBB_360_안테나_정렬.py
@@ -11,4 +11,14 @@ for house in arr:
 
 distance.sort(key = lambda x: (x[1], x[0]))
 
+print(distance)
 print(distance[0][0])
+
+# 상당히 원시적인 방법
+# 수학적으로 생각하면, 모든 집들의 위치를 받은 뒤 해당 집들의 가장 중간값을 리턴해주면 된다.
+#
+# arr.sort()
+#
+# print(arr[(len(arr) - 1) // 2])
+
+# 값이 홀수이면 무조건 중앙, 값이 짝수이면 왼쪽에 있는 것. 위의 식으로 돌려보면 어떻게든 같은 값이 2개 나오게 된다.
