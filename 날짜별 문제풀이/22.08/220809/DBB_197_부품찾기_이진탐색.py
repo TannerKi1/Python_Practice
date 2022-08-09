057c5d4d1d85fbc5c5262fb3351a2eebb59cbf2a
Tanner <monokase18@gmail.com>
2022-08-09 09:10:29 +0900

    New Problem

---
+++ 날짜별 문제풀이/22.08/220809/DBB_197_부품찾기_이진탐색.py
@@ -0,0 +1,30 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return mid
+
+        elif array[mid] > target:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    return None # while문을 다 돌았는데도 없으면 이 되려면 여기 위치에 indent가 있어야함.
+
+
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+
+m = int(input())
+item = list(map(int, input().split()))
+
+for i in item:
+    result = binary_search(array, i, 0, n-1)
+    if result is not None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
