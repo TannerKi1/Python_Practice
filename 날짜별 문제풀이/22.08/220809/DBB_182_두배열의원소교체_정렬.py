0eb9b5a469804a1a632bea26d9368b1a74e1f09e
Tanner <monokase18@gmail.com>
2022-08-09 11:28:23 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220809/DBB_182_두배열의원소교체_정렬.py
+++ 날짜별 문제풀이/22.08/220809/DBB_182_두배열의원소교체_정렬.py
@@ -10,7 +10,7 @@ max_arr.sort(reverse=True)
 life = k
 
 for i in range(n):
-    if min_arr[i] < max_arr[i]:
+    if min_arr[i] < max_arr[i]: # 핵심조건. 작은 경우에만 바꿔줘야함.
         min_arr[i] = max_arr[i]
         life -= 1
 
