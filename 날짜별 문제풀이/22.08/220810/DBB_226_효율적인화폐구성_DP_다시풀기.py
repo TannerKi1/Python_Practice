bd0d500efa6a9db5bc46974549afee17f2df96fe
Tanner <monokase18@gmail.com>
2022-08-10 08:37:06 +0900

    New Problem

--- 날짜별 문제풀이/22.08/220810/DBB_226_효율적인화폐구성_DP_다시풀기.py
+++ 날짜별 문제풀이/22.08/220810/DBB_226_효율적인화폐구성_DP_다시풀기.py
@@ -8,7 +8,7 @@ for _ in range(N):
 d = [9999] * (M + 1)
 d[0] = 0
 for i in range(len(coin)):
-    for j in range(coin[i], M+1):
+    for j in range(coin[i], M+1): # j의 시작점이 coin[i]라는거, 그리고 작은 코인부터 나열되어야한다는거
         d[j] = min(d[j], d[j - coin[i]] + 1)
 
 if d[M] != 9999:
