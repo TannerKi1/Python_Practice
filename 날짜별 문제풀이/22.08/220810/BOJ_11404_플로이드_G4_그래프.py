9e3aa44d8dcdbf063372a6fe0410980778bdba12
Tanner <monokase18@gmail.com>
2022-08-10 14:19:58 +0900

    New Problem

---
+++ 날짜별 문제풀이/22.08/220810/BOJ_11404_플로이드_G4_그래프.py
@@ -0,0 +1,25 @@
+n = int(input())
+m = int(input())
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c #노선은 하나가 아닐 수 있다. 어떻게 업데이트 할까?
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print('INF', end=' ')
+        else:
+            print(graph[a][b], end=' ')
+    print()
