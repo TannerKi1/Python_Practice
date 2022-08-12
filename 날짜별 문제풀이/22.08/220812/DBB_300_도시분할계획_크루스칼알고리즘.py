5bb6cc34087e5f87db8ba5879b957a7444cef446
Tanner <monokase18@gmail.com>
2022-08-12 11:14:02 +0900

    New Problem

---
+++ 날짜별 문제풀이/22.08/220812/DBB_300_도시분할계획_크루스칼알고리즘.py
@@ -0,0 +1,46 @@
+N, M = map(int, input().split())
+
+parent = [0] * (N+1)
+
+for i in range(1, N+1):
+    parent[i] = i # 집합노드
+
+
+edges = [] # 모든 도로들의 정보가 들어가있음.
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+### 사전 준비 완료
+
+### 함수 준비 시작
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+total = 0
+result = []
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        total += cost
+        result.append(cost)
+        union(parent, a, b)
+
+print(total - result[-1])
+
