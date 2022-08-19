b8618a5897d160ea108a620c1ad21f0eb64ccfcc
Tanner <monokase18@gmail.com>
2022-08-19 19:18:12 +0900

    New Problem

--- Daily Algorithm/22.08/220819/BOJ_7562_나이트의이동_S1_03DFSBFS.py
+++ Daily Algorithm/22.08/220819/BOJ_7562_나이트의이동_S1_03DFSBFS.py
@@ -1,4 +1,42 @@
-T = int(input())
+from collections import deque
 
+dx = [+2, +1, -1, -2, -2, -1, +1, +2]
+dy = [+1, +2, +2, +1, -1, -2, -2, -1]
+
+
+
+def knight_move(x, y, sx, sy):
+
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        curX, curY = queue.popleft()
+        if curX == sx and curY == sy:
+            print(graph[curX][curY] - 1)
+            return True
+
+        for i in range(8):
+            nx = curX + dx[i]
+            ny = curY + dy[i]
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                continue
+
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[curX][curY] + 1
+                queue.append((nx, ny))
+    return False
+
+T = int(input())
 for _ in range(T):
     N = int(input())
+    graph = [[0] * N for _ in range(N)]
+    x, y = map(int, input().split())
+    sx, sy = map(int, input().split())
+    graph[x][y] = 1
+    knight_move(x, y, sx, sy)
+
+
+
+
---
+++ Daily Algorithm/22.08/220819/이웃하지않게않기.py
@@ -0,0 +1,31 @@
+def solution(idx, n, lst):
+    global answer
+
+    if idx == n:
+        print(lst)
+        answer += 1
+        return
+
+    if idx == 0:
+        lst[idx] = True
+        solution(idx + 1, n, lst)
+        lst[idx] = False
+        solution(idx + 1, n, lst)
+
+    else:
+        if lst[idx - 1] == True:
+            lst[idx] = False
+            solution(idx + 1, n, lst)
+        else:
+            lst[idx] = True
+            solution(idx + 1, n, lst)
+            lst[idx] = False
+            solution(idx + 1, n, lst)
+            lst[idx] = "나는 고양이"
+            solution(idx + 1, n, lst)
+
+answer = 0
+n = 3
+solution(0, n, [[None] for _ in range(n)])
+
+print(answer)
