6e823dc6cb9ef47def5abaf2c7d131474d1d214d
Tanner <monokase18@gmail.com>
2022-08-23 11:58:48 +0900

    New Problem

--- Daily Algorithm/22.08/220823/LC_200_NumberOfIslands_03DFSBFS.py
+++ Daily Algorithm/22.08/220823/LC_200_NumberOfIslands_03DFSBFS.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 grid = [
   ["1","1","1","1","0"],
   ["1","1","0","1","0"],
@@ -8,27 +10,83 @@ grid = [
 rows = len(grid)
 cols = len(grid[0])
 
+def bfs(x, y):
 
-def dfs(x, y):
+    queue = deque()
+    queue.append((x, y))
 
-    if x < 0 or x >= rows or y < 0 or y >= cols:
-        return False
+    while queue:
+        r, c = queue.popleft()
 
-    if grid[x][y] == '1':
-        grid[x][y] = 0
-        dfs(x - 1, y)
-        dfs(x + 1, y)
-        dfs(x, y - 1)
-        dfs(x, y + 1)
-        return True
-    return False
+        dr = [1, -1, 0, 0]
+        dc = [0, 0, 1, -1]
 
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
+                continue
+
+            if grid[nr][nc] == '1':
+                grid[nr][nc] = 0
+                queue.append((nr, nc))
 
 cnt = 0
 for r in range(rows):
     for c in range(cols):
-        if dfs(r, c):
+        if grid[r][c] == '1':
+            grid[r][c] = 0
+            bfs(r, c)
             cnt += 1
 
 print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def dfs(x, y):
+#
+#     if x < 0 or x >= rows or y < 0 or y >= cols:
+#         return False
+#
+#     if grid[x][y] == '1':
+#         grid[x][y] = 0
+#         dfs(x - 1, y)
+#         dfs(x + 1, y)
+#         dfs(x, y - 1)
+#         dfs(x, y + 1)
+#         return True
+#     return False
+#
+#
+# cnt = 0
+# for r in range(rows):
+#     for c in range(cols):
+#         if dfs(r, c):
+#             cnt += 1
+#
+# print(cnt)
 
